b02c24f021a3ada6ae5bd03b8f819944ab0b18cb
Jason <1123150116@global.ac.id>
2026-07-02 05:24:23 +0700

    buat dan isi product_model.dart

---
+++ mobile/lib/features/catalog/model/product_model.dart
@@ -0,0 +1,11 @@
+class ProductModel {
+  final String id;
+  final String name;
+  final int price;
+
+  ProductModel({
+    required this.id,
+    required this.name,
+    required this.price,
+  });
+}
