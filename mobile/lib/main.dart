7e69634f7a0c5653e342aab63fc995a5cc45bc41
Jason <1123150116@global.ac.id>
2026-07-02 05:27:01 +0700

    update main.dart

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:provider/provider.dart';
 
 import 'features/auth/provider/auth_provider.dart';
 import 'features/auth/presentation/login_page.dart';
+import 'features/cart/provider/cart_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,6 +24,7 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => CartProvider()),
       ],
       child: const MaterialApp(
         debugShowCheckedModeBanner: false,
