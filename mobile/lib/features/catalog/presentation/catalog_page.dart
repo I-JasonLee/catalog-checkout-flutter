5bedd759eeb5d0bfbe35cee3c0039416a7f00c30
Jason <1123150116@global.ac.id>
2026-07-02 05:31:20 +0700

    buat dan isi catalog_page.dart

---
+++ mobile/lib/features/catalog/presentation/catalog_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../cart/provider/cart_provider.dart';
+import '../../auth/model/product_model.dart';
+
+class CatalogPage extends StatelessWidget {
+  CatalogPage({super.key});
+
+  final List<ProductModel> products = [
+    ProductModel(id: "1", name: "Mouse Gaming", price: 150000),
+    ProductModel(id: "2", name: "Keyboard Mechanical", price: 350000),
+    ProductModel(id: "3", name: "Headset", price: 200000),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final cart = Provider.of<CartProvider>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Catalog"),
+      ),
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          final item = products[index];
+
+          return Card(
+            child: ListTile(
+              title: Text(item.name),
+              subtitle: Text("Rp ${item.price}"),
+              trailing: ElevatedButton(
+                onPressed: () {
+                  cart.addToCart(item);
+                },
+                child: const Text("Add"),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
