ae9ff8a5e731ce69d5ba1e29f541769ebbd32e5b
Jason <1123150116@global.ac.id>
2026-07-02 05:33:07 +0700

    buat dan isi cart_page.dart

---
+++ mobile/lib/features/cart/provider/presentation/cart_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../provider/cart_provider.dart';
+
+class CartPage extends StatelessWidget {
+  const CartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final cart = Provider.of<CartProvider>(context);
+
+    return Scaffold(
+      appBar: AppBar(title: const Text("Cart")),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: cart.items.length,
+              itemBuilder: (context, index) {
+                final item = cart.items[index];
+
+                return ListTile(
+                  title: Text(item.name),
+                  subtitle: Text("Rp ${item.price}"),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () {
+                      cart.removeFromCart(item);
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+
+          Container(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                Text(
+                  "Total: Rp ${cart.totalPrice}",
+                  style: const TextStyle(fontSize: 20),
+                ),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    cart.clearCart();
+
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text("Checkout Success")),
+                    );
+                  },
+                  child: const Text("Checkout"),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
