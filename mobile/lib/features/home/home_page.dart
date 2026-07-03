d481a9f85da94ab74562303951976ecaf7b51df0
Jason <1123150116@global.ac.id>
2026-07-03 23:54:52 +0700

    update home_page.dart

--- mobile/lib/features/home/home_page.dart
+++ mobile/lib/features/home/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
 import '../catalog/presentation/catalog_page.dart';
+import '../cart/presentation/cart_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -8,15 +8,26 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Home")),
+      appBar: AppBar(
+        title: const Text("Home"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => const CartPage()),
+              );
+            },
+          )
+        ],
+      ),
       body: Center(
         child: ElevatedButton(
           onPressed: () {
             Navigator.push(
               context,
-              MaterialPageRoute(
-                builder: (_) => const CatalogPage(),
-              ),
+              MaterialPageRoute(builder: (_) => const CatalogPage()),
             );
           },
           child: const Text("Go to Catalog"),
