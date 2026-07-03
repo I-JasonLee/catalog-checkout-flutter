e68cc824bae83fbf95f7cedeb27a94ba101592b0
Jason <1123150116@global.ac.id>
2026-07-03 23:00:53 +0700

    update product_model.dart

--- mobile/lib/features/catalog/model/product_model.dart
+++ mobile/lib/features/catalog/model/product_model.dart
@@ -2,10 +2,12 @@ class ProductModel {
   final String id;
   final String name;
   final int price;
+  final String? image;
 
   ProductModel({
     required this.id,
     required this.name,
     required this.price,
+    this.image,
   });
 }
