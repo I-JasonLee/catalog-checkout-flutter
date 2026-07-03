79e87dfe64be7acfa65a3187910e982d01c02109
Jason <1123150116@global.ac.id>
2026-07-03 23:31:46 +0700

    Add Cart Provider and integrate with Catalog

--- mobile/lib/features/catalog/presentation/catalog_page.dart
+++ mobile/lib/features/catalog/presentation/catalog_page.dart
@@ -1,35 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../data/product_dummy.dart';
 import '../model/product_model.dart';
+import '../../cart/provider/cart_provider.dart';
 
 class CatalogPage extends StatelessWidget {
   const CatalogPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final List<ProductModel> products = ProductDummy.products;
+    final products = ProductDummy.products;
+    final cart = Provider.of<CartProvider>(context);
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Catalog Product"),
+        title: const Text("Catalog"),
       ),
       body: ListView.builder(
         itemCount: products.length,
         itemBuilder: (context, index) {
           final product = products[index];
 
-          return Card(
-            margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-            child: ListTile(
-              title: Text(product.name),
-              subtitle: Text("Rp ${product.price}"),
-              trailing: const Icon(Icons.add_shopping_cart),
-              onTap: () {
+          return ListTile(
+            title: Text(product.name),
+            subtitle: Text("Rp ${product.price}"),
+            trailing: IconButton(
+              icon: const Icon(Icons.add),
+              onPressed: () {
+                cart.addToCart(product);
                 ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text("${product.name} dipilih"),
-                  ),
+                  SnackBar(content: Text("${product.name} added")),
                 );
               },
             ),
